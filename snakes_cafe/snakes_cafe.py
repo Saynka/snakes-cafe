f74274a203437c4c5be8b1abe6d91039d53e2273
Saynka <sankakale@gmail.com>
2020-12-06 16:17:16 -0800

    placeholder

---
+++ snakes_cafe/snakes_cafe.py
@@ -0,0 +1,69 @@
+from textwrap import dedent
+
+
+food = {
+  'wings': 0,
+  'cookies': 0,
+  'spring rolls': 0,
+  'salmon': 0,
+  'steak': 0,
+  'meat tornado': 0,
+  'a literal garden': 0,
+  'ice cream': 0,
+  'cake': 0,
+  'pie': 0,
+  'coffee': 0,
+  'tea': 0,
+  'unicorn tears': 0,
+}
+
+
+def snakes():
+
+
+
+    appstring = """
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+    """
+
+
+    print(appstring)
+
+
+
+    message = """
+***********************************
+** What would you like to order? **
+***********************************
+    """
+ 
