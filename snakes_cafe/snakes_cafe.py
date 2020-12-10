1b93cfdb94f6da60be59dd0aba43f03c5c272d46
Saynka <sankakale@gmail.com>
2020-12-09 17:57:47 -0800

    workingmenu

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -2,19 +2,19 @@ from textwrap import dedent
 
 
 food = {
-  'wings': 0,
-  'cookies': 0,
-  'spring rolls': 0,
-  'salmon': 0,
-  'steak': 0,
-  'meat tornado': 0,
-  'a literal garden': 0,
-  'ice cream': 0,
-  'cake': 0,
-  'pie': 0,
-  'coffee': 0,
-  'tea': 0,
-  'unicorn tears': 0,
+    'wings': 0,
+    'cookies': 0,
+    'spring rolls': 0,
+    'salmon': 0,
+    'steak': 0,
+    'meat tornado': 0,
+    'a literal garden': 0,
+    'ice cream': 0,
+    'cake': 0,
+    'pie': 0,
+    'coffee': 0,
+    'tea': 0,
+    'unicorn tears': 0,
 }
 
 
@@ -23,37 +23,37 @@ def snakes():
 
 
     appstring = """
-**************************************
-**    Welcome to the Snakes Cafe!   **
-**    Please see our menu below.    **
-**
-** To quit at any time, type "quit" **
-**************************************
+    **************************************
+    **    Welcome to the Snakes Cafe!   **
+    **    Please see our menu below.    **
+    **
+    ** To quit at any time, type "quit" **
+    **************************************
 
-Appetizers
-----------
-Wings
-Cookies
-Spring Rolls
+    Appetizers
+    ----------
+    Wings
+    Cookies
+    Spring Rolls
 
-Entrees
--------
-Salmon
-Steak
-Meat Tornado
-A Literal Garden
+    Entrees
+    -------
+    Salmon
+    Steak
+    Meat Tornado
+    A Literal Garden
 
-Desserts
---------
-Ice Cream
-Cake
-Pie
+    Desserts
+    --------
+    Ice Cream
+    Cake
+    Pie
 
-Drinks
-------
-Coffee
-Tea
-Unicorn Tears
+    Drinks
+    ------
+    Coffee
+    Tea
+    Unicorn Tears
     """
 
 
@@ -62,8 +62,36 @@ Unicorn Tears
 
 
     message = """
-***********************************
-** What would you like to order? **
-***********************************
-    """
- 
+    ***********************************
+    ** What would you like to order? **
+    ***********************************
+        """
+
+    userorder(message)
+
+
+def userorder(message=''):
+
+
+    dedent(message)
+
+
+    user_input = input(message).lower()
+
+    if user_input == 'quit':
+        pass
+    else:
+        while user_input not in food.keys():
+            message = dedent("""
+            Thats not on the menu please choose an item on the menu!!!
+            """)
+            user_input = input(message).lower()
+        food[user_input] += 1
+
+        print(f"{food[user_input]} order of %s has been added to your meal" % user_input)
+
+        userorder()
+
+
+if __name__ == '__main__':
+    snakes()
